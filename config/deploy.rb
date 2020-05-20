698f5d111f44cca3a7dbe7c8267fb273a7a827c9
perunari <per.roc.ski@gmail.com>
2020-05-20 16:37:37 +0900

    deploy set2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "freemarket_sample_75f"
-set :repo_url, "git@example.com:perunari/freemarket_sample_75f.git"
+set :repo_url, "git@github.com:perunari/freemarket_sample_75f.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/deploy.test.pem']
+                  keys: ['~/.ssh/deploytest.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
