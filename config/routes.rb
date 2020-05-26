7805bd7f9b07c4618f7865533fddee4f453ded15
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-05-26 19:49:32 +0900

    マイページとログアウトのルートとCSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'items#index'
+  resources :users, only: :show
+  get '/logout', to: 'users#index'
   resources :items, only: [:new, :edit, :create, :show]
   resources :credit_cards, only: :new
 end
