da13e16ce8b45397f9215e2e655b0b0619dc9512
perunari <per.roc.ski@gmail.com>
2020-05-26 10:50:15 +0900

    routes items edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items,only: :new
+  resources :items, only: [:new, :edit]
   resources :credit_cards, only: :new
 end
