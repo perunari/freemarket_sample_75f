291cc52602be520fbba1432cc629ab6891709132
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-05-26 19:05:14 +0900

    remake my-page & log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#show"
-  resource :items, only: [:edit, :create, :show]
-  resource :credit_cards, only: :new
+  resources :items, only: [:edit, :create, :show]
+  resources :credit_cards, only: :new
   resources :users, only: :edit
   get "/logout", to: "users#index"
 end
