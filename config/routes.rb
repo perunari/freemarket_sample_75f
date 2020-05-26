ec88799a4883169e182e1d14813163501f25ca03
perunari <per.roc.ski@gmail.com>
2020-05-26 16:18:10 +0900

    Revert "Merge pull request #8 from perunari/user--mypage"
    This reverts commit 4ce13722fad2dbd69c16103394a66b7e3b757998, reversing
    changes made to f1eedeb8d9a372ee35d1a3349dd91eab57ec778f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#show"
-  resource :items, only: [:index, :edit, :create, :show]
+  root 'items#index'
+  resource :items, only: [:edit, :create, :show]
   resource :credit_cards, only: :new
-  resource :users, only: :edit
-  get "/logout", to: "users#index"
-
 end
