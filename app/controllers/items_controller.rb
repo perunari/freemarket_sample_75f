8ee68d4700f3d49b1bb6d297febf3b092a7fdd50
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-05-27 21:57:04 +0900

    set image and item model, items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,32 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.images.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+  end
+
   def show
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payment, :ship_from, :preparation, :price, images_attributes: [:picture]).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :buying, dependent: :destroy
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
+  belongs_to :category
+  belongs_to :brand
+
+  validates :user_id, :name, :description, :category_id, :condition, :postage_payment, :ship_from, :preparation, :price, presence: true
+  validates :name, length: { maximum: 40 }
+  validates :description, length: { maximum: 1000 }
+  # validates :images, length: { minimum: 1 }
+    
 end
