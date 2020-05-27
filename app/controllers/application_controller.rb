8323d07176f2d6382f8b6f8e61fb66a73e6d8ea5
koshio3112 <ponyo0813131@gmail.com>
2020-05-27 19:47:17 +0900

    signup doing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   private
 
   def basic_auth
@@ -14,4 +14,9 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, :postal_code, :prefecture, :city, :block, :building, :phone])
+  end
 end
