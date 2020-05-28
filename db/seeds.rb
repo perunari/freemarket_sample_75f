341393285cd48d47f4536c423d25ed3fcdebf764
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-05-28 16:02:36 +0900

    wrriting record

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,97 @@
-lady = Category.create(:name=>"レディース")
-
-lady_tops = lady.children.create(:name=>"トップス")
-lady_jacket = lady.children.create(:name=>"ジャケット/アウター")
-
-lady_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"その他"}])
-lady_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"その他"}])
+categories=[
+           {level1:"レディース",level1_children:[
+                                               {level2:"トップス",level2_children:[Tシャツ/カットソー(半袖/袖なし)Tシャツ/カットソー(七分/長袖)シャツ/ブラウス(半袖/袖なし)シャツ/ブラウス(七分/長袖)ポロシャツキャミソール]},
+                                               {level2:"ジャケット/アウター",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"パンツ",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"スカート",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"ワンピース",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"靴",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"ルームウェア/パジャマ",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"レッグウェア",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"帽子",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"バッグ",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"アクセサリー",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"ヘアアクセサリー",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"小物",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"時計",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"ウィッグ/エクステ",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"浴衣/水着",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"スーツ/フォーマル/ドレス",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"マタニティ",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"その他",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           },
+           {level1:"メンズ",level1_children:[
+                                               {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+                                               {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+                                              ]
+           },
+           {level1:"ベビー・キッズ",level1_children:[
+                                               {level2:"カテゴリ 1-1",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"カテゴリ 1-2",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           },
+           {level1:"インテリア・住まい・小物",level1_children:[
+                                               {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+                                               {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+                                              ]
+           },
+           {level1:"本・音楽・ゲーム",level1_children:[
+                                               {level2:"カテゴリ 1-1",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"カテゴリ 1-2",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           },
+           {level1:"おもちゃ・ホビー・グッズ",level1_children:[
+                                               {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+                                               {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+                                              ]
+           },
+           {level1:"コスメ・香水・美容",level1_children:[
+                                               {level2:"カテゴリ 1-1",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"カテゴリ 1-2",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           },
+           {level1:"家電・スマホ・カメラ",level1_children:[
+                                               {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+                                               {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+                                              ]
+           },
+           {level1:"スポーツ・レジャー",level1_children:[
+                                               {level2:"カテゴリ 1-1",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"カテゴリ 1-2",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           },
+           {level1:"ハンドメイド",level1_children:[
+                                               {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+                                               {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+                                              ]
+           },
+           {level1:"チケット",level1_children:[
+                                               {level2:"カテゴリ 1-1",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"カテゴリ 1-2",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           },
+           {level1:"自動車・オートバイ",level1_children:[
+                                               {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+                                               {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+                                              ]
+           },
+           {level1:"その他",level1_children:[
+                                               {level2:"カテゴリ 1-1",level2_children:["カテゴリ 1-1-1","カテゴリ 1-1-2","カテゴリ 1-1-3"]},
+                                               {level2:"カテゴリ 1-2",level2_children:["カテゴリ 1-2-1","カテゴリ 1-2-2"]}
+                                              ]
+           }
+         ]
+categories.each.with_index(1) do |category,i|
+ level1_var="@category#{i}"                                                        #1階層の変数("@category1"等)
+ level1_val= Category.create(name:"#{category[:level1]}")                          #1階層の値作成("カテゴリ　1"等)
+ eval("#{level1_var} = level1_val")                                                #1階層の変数=1階層の値
+   category[:level1_children].each.with_index(1) do |level1_child,j|
+     level2_var="#{level1_var}_#{j}"                                               #2階層の変数("@category1-1"等)
+     level2_val= eval("#{level1_var}.children.create(name:level1_child[:level2])") #2階層の値作成("カテゴリ　1-1"等)
+     eval("#{level2_var} = level2_val")                                            #2階層の変数=2階層の値
+       level1_child[:level2_children].each do |level2_children_val|
+         eval("#{level2_var}.children.create(name:level2_children_val)")           #3階層の値作成("カテゴリ　1-1-1"等)
+       end
+   end
+end
