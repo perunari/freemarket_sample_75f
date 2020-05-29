e5d92f6c4355a25422fd7e9b362558b2c23a246e
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-05-29 16:59:11 +0900

    comp main categories view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
 
   def index
-    @parents = Category.all.order("id ASC").limit(13)
+    @category = Category.where(ancestry: nil)
+    
   end
 
 end
