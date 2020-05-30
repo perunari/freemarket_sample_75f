3ba87a60b49a48b8d7022d7172ea8aa63d0871a9
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-05-30 14:42:13 +0900

    create pull down select

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,15 @@ class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
-  belongs_to :brand
+  belongs_to :brand, optional: true
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payment
+  belongs_to_active_hash :preparation
 
-  validates :user_id, :name, :description, :category_id, :condition, :postage_payment, :ship_from, :preparation, :price, presence: true
+  validates :user_id, :name, :description, :category_id, :condition_id, :postage_payment_id, :ship_from_id, :preparation_id, :price, presence: true
   validates :name, length: { maximum: 40 }
   validates :description, length: { maximum: 1000 }
   # validates :images, length: { minimum: 1 }
