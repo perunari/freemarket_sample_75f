be57b6b058aff2fbc8981cabf92cca1b61da3e4a
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-05-30 14:42:55 +0900

    update items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payment, :ship_from, :preparation, :price, images_attributes: [:picture]).merge(user_id: current_user.id)
+    # params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payment, :ship_from, :preparation, :price, images_attributes: [:picture]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payment, :ship_from, :preparation, :price, images_attributes: [:picture])
   end
 
 end
