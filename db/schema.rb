3a54d882140ba213da0bcc189463cf57f2994886
perunari <per.roc.ski@gmail.com>
2020-05-31 16:58:38 +0900

    schema commit

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 2020_05_31_073402) do
     t.string "block", null: false
     t.string "building"
     t.integer "phone"
+    t.integer "prefecture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "prefecture_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -90,10 +90,10 @@ ActiveRecord::Schema.define(version: 2020_05_31_073402) do
     t.text "description", null: false
     t.bigint "category_id"
     t.bigint "brand_id"
-    t.integer "condition_id", null: false
-    t.integer "postage_payment_id", null: false
-    t.integer "ship_from_id", null: false
-    t.integer "preparation_id", null: false
+    t.string "condition_id", null: false
+    t.string "postage_payment_id", null: false
+    t.string "ship_from_id", null: false
+    t.string "preparation_id", null: false
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
