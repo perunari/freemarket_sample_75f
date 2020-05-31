610cd42beb50c3c96f7e915363e94f2e5a149201
koshio3112 <ponyo0813131@gmail.com>
2020-05-31 20:24:29 +0900

    item_category model削除

--- db/migrate/20200531073402_create_item_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateItemCategories < ActiveRecord::Migration[5.2]
-  def change
-    create_table :item_categories do |t|
-      t.references :item, type: :bigint, foreign_key: true
-      t.references :category, type: :bigint, foreign_key: true
-      t.timestamps
-    end
-  end
-end
