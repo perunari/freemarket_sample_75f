aa307d25ebe87fc774a4e217d0d7fb3fc2ae302b
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-05-31 12:45:57 +0900

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :users, only: :show
   get '/logout', to: 'users#index'
-  resources :items, only: [:new, :create, :edit, :update, :show]
+  resources :items, only: [:new, :create, :edit, :update, :show] do
+    collection do
+      get 'get_child_categories', defaults: { format: 'json' }
+      get 'get_grandchild_categories', defaults: { format: 'json' }
+    end
+  end
   resources :credit_cards, only: :new
   resources :buyings, only: [:new, :create]
 end
