c25a8d57409a0a6815a0e6be8d64fa11e9884755
koshio3112 <ponyo0813131@gmail.com>
2020-06-01 10:34:39 +0900

    登録途中のエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
+    post '/users/sign_up', to: 'users/registrations#create'
   end
   
   root 'top#index'
