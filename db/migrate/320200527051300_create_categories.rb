1197e82dd2ef31245f081cf4958de2fe2c7849f7
perunari <per.roc.ski@gmail.com>
2020-06-01 21:53:06 +0900

    dbの順番修正

--- db/migrate/320200527051300_create_categories.rb
+++ db/migrate/320200527051300_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
-    create_table :categories do |t|
+    create_table :categories, id: :integer do |t|
       t.string :name, null: false
-      t.integer :parent_id
+      t.string :ancestry
       t.timestamps
     end
   end
