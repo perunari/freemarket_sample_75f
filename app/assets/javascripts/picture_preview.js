bc628b906db36637af26474abadf5ccd0c3a4840
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-01 19:37:14 +0900

    create image up UI

--- app/assets/javascripts/picture_preview.js
+++ app/assets/javascripts/picture_preview.js
@@ -9,24 +9,23 @@ $(function() {
         <div class='item-image' data-image="${file.name}">
           <div class='item-image__content'>  
             <div class='item-image__content--icon'>
-              <img src="${src}" width='100' height='100'>
-              <div class='item-image__operation'>
-                <div class='item-image__operation--delete'>削除</div>
-              </div>
+              <img src="${src}" width='80' height='120'>
             </div>
+          </div>
+          <div class='item-image__operation'>
+            <div class='item-image__operation--delete'>削除</div>
           </div>
         </div>
       `;
-      $('.image-upload__box').before(html);
+      $('.image-upload').before(html);
     };
     fileReader.readAsDataURL(file);
   });
 
   $(document).on('click', '.item-image__operation--delete', function() {
-    const target_image = $(this).parent().parent().parent().parent();
+    const target_image = $(this).parent().parent();
     target_image.remove();
     file_field.val('');
   });
-
 
 });
