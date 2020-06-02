39ada075570d83243b7373743e04164ee7452473
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-02 21:39:36 +0900

    upload picture

--- app/assets/javascripts/picture_preview.js
+++ app/assets/javascripts/picture_preview.js
@@ -1,31 +1,62 @@
 $(function() {
+
+  const dataBox = new DataTransfer();
   const file_field = document.querySelector('input[type="file"]');
+  
   $('.file-input').on('change', function() {
     const file = $('input[type="file"]').prop('files')[0];
-    const fileReader = new FileReader();
-    fileReader.onloadend = function() {
-      const src = fileReader.result;
-      const html = `
-        <div class='item-image' data-image="${file.name}">
-          <div class='item-image__content'>  
-            <div class='item-image__content--icon'>
-              <img src="${src}" width='80' height='120'>
+
+    $.each(this.files, function(i, file) {
+      const fileReader = new FileReader();
+      dataBox.items.add(file);
+      file_field.files = dataBox.files;
+      let num = $('.item-image').length + 1 + i;
+      fileReader.readAsDataURL(file);
+
+      if (num == 10) {
+        $('#image-box__container').css('display', 'none');
+      }
+
+      fileReader.onloadend = function() {
+        const src = fileReader.result;
+        const html = `
+          <div class='item-image' data-image="${file.name}">
+            <div class='item-image__content'>  
+              <div class='item-image__content--icon'>
+                <img src="${src}" width='80' height='120'>
+              </div>
+            </div>
+            <div class='item-image__operation'>
+              <div class='item-image__operation--delete'>削除</div>
             </div>
           </div>
-          <div class='item-image__operation'>
-            <div class='item-image__operation--delete'>削除</div>
-          </div>
-        </div>
-      `;
-      $('.image-upload').before(html);
-    };
-    fileReader.readAsDataURL(file);
+        `;
+        $('#image-box__container').before(html);
+      };
+      $('#image-box__container').attr('class', `item-num-${num}`);
+    });
   });
 
   $(document).on('click', '.item-image__operation--delete', function() {
     const target_image = $(this).parent().parent();
-    target_image.remove();
-    file_field.val('');
-  });
+    const target_name = $(target_image).data('image');
 
+    if (file_field.files.length == 1) {
+      $('input[type="file"]').val(null);
+      dataBox.clearData();
+      console.log(dataBox);
+    } else {
+      $.each(file_field.files, function(i, input) {
+        if (input.name == target_name) {
+          dataBox.items.remove(i);
+        }
+      });
+      file_field.files = dataBox.files;
+    }
+
+    target_image.remove();
+    let num = $('.item-image').length;
+    $('#image-box__container').show();
+    $('#image-box__container').attr('class', `item-num-${num}`);
+  });
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
-  before_action :set_item_and_images, only: [:new, :create]
   before_action :set_parent_categories, only: [:new, :create]
 
   def index
   end
 
   def new
+    @item = Item.new
+    @item.images.build
   end
 
   def create
@@ -13,6 +14,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+      @item.images.build
       render :new
     end
   end
@@ -45,11 +47,6 @@ class ItemsController < ApplicationController
   def item_params
     # params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition_id, :postage_payment_id, :ship_from_id, :preparation_id, :price, images_attributes: [:picture]).merge(user_id: current_user.id)
     params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition_id, :postage_payment_id, :ship_from_id, :preparation_id, :price, images_attributes: [:picture]).merge(user_id: 1)
-  end
-
-  def set_item_and_images
-    @item = Item.new
-    @images = @item.images.new
   end
 
   def set_parent_categories
