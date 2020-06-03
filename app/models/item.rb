00c47a7c25876307b4d6d260829cd4bd456f8d24
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-03 19:30:57 +0900

    add status_id column to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :status
+
+  attribute :status_id, :integer, default: 1
+  varidates :status_id, presence: true
 end
