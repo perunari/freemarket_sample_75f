672ce8f1227a79b472c770b67dcb3aff61fc5d21
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-06 15:58:04 +0900

    solved the problem of 2 way error messages for cateogory

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   has_one :buying, dependent: :destroy
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :category
+  belongs_to :category, optional: true
   belongs_to :brand, optional: true
   
   extend ActiveHash::Associations::ActiveRecordExtensions
