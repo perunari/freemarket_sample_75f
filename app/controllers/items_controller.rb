f7cc633709d37b1c4eb60d2793b9a2a33dcd18b0
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-06 15:59:06 +0900

    keep the selected category after rendering new and edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_parent_categories, only: [:new, :create]
+  before_action :set_parent_categories, only: [:new, :create, :edit, :update]
   before_action :signed_in?, only: [:new, :create]
 
   def index
@@ -8,23 +8,43 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.build
+    @item.price = 0
   end
 
   def create
+    params[:item][:category_id] = "" if params[:parent_category].blank? || params[:child_category].blank?
     @item = Item.new(item_params)
-    @item.user_id = current_user.id
+
     if @item.save
       redirect_to root_path
     else
       @item.images.build
+      @parent_category = params[:parent_category] if params[:parent_category].present?
+      @child_category = params[:child_category] if params[:child_category].present?
+      @grandchild_category = params[:item][:category_id] if params[:item][:category_id].present?
       render :new
     end
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @child_category = @item.category.parent
+    @parent_category = @child_category.parent
   end
 
   def update
+    params[:item][:category_id] = "" if params[:parent_category].blank? || params[:child_category].blank?
+    @item = Item.find(params[:id])
+    binding.pry
+    
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      @parent_category = params[:parent_category] if params[:parent_category].present?
+      @child_category = params[:child_category] if params[:child_category].present?
+      @grandchild_category = params[:item][:category_id] if params[:item][:category_id].present?
+      render :edit
+    end
   end
 
   def show
@@ -47,7 +67,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition_id, :postage_payment_id, :ship_from_id, :preparation_id, :price, images_attributes: [:picture]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition_id, :postage_payment_id, :ship_from_id, :preparation_id, :price, images_attributes: [:picture, :_destroy, :id]).merge(user_id: current_user.id)
   end
 
   def set_parent_categories
