64a110374b75801e4cc1b2f249d88e52d000e3f8
koshio3112 <ponyo0813131@gmail.com>
2020-06-08 19:44:43 +0900

    マイページ　編集

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 2020_06_07_120611) do
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "size"
     t.integer "status_id"
     t.string "size"
     t.index ["brand_id"], name: "index_items_on_brand_id"
