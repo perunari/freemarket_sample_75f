96f70a131d8b97ef4d31c6847add88197c3ea849
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-09 13:20:56 +0900

    create payjp and buying function

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,2 +1,6 @@
 class CreditCard < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :buyings, dependent: :destroy
+
+  validates :user_id, :customer_id, :card_id, presence: true
 end
